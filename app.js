f13ba82a5325c482c58ae81ed1f330d0576e6821
AbdulMoiza9257 <abdulmoiza9257@gmail.com>
2023-12-08 14:28:23 -0800

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+var age = 18;
+var cinc = true;
+var studentcard = true;
+if (age >= 18 && studentcard == true) {
+  console.log("allow");
+} else {
+  console.log("not allow");
+}
+if (age >= 18 || studentcard == true) {
+  console.log("allow");
+}
+
+var a = "a";
+var a = "b";
+var arr = ["a", "b", "c", "d", "e"];
+// console.log(arr);
+// arr.push("f");
+// arr.pop ();
+// console.log (arr)
+// console.log ()
+
+var arr=arr.slice(1,3)
+console.log (arr);
+
+// var arr=arr.splice(1,3)
+// console.log (arr);
+
